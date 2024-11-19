8fb641dcedc23eff1ead3c67d298a7d301bd10ba
judejoyson <156394555+judejoyson@users.noreply.github.com>
2024-11-19 15:00:18 -0500

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Average {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		double[] values = new double[10]; 
+		System.out.print("Enter ten numbers that are double: ");
+		for (int i = 0; i < values.length; i++) {
+			values[i] = input.nextDouble();
+		}
+
+		System.out.println("The average value is " + average(values));
+	}
+
+	public static int average(int[] array) {
+		int average = 0; 
+		for (int i: array)
+			average += i;
+		return average / array.length;
+	}   
+
+	public static double average(double[] array) {
+		double average = 0; 
+		for (double i: array)
+			average += i; 
+		return average / array.length;
+	}
+}
