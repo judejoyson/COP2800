f51019433e3a8ee92070309367195b6320047d75
judejoyson <156394555+judejoyson@users.noreply.github.com>
2024-12-01 22:59:02 -0500

    Update HexagonArea.java

--- HexagonArea.java
+++ HexagonArea.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class HexagonArea {
     public static void main(String[] args) throws Exception {
-        double side, area, numeratorArea, denominatorArea;
+        double side, area;
         System.out.println("This program will calcluate the area of a hexagon.");
         System.out.println("Please enter the side length");
         Scanner user = new Scanner(System.in);
         side = user.nextDouble();
         area = (6 * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / 6));
-        System.out.printf("The are of the hexagon is"+ area);
-
-
+        System.out.printf("The area of the hexagon is %.2f%n", area);
+        user.close();
 
     }
 }
